c43cb2fa8b0ec0632972e02bfcf99381b91683bb
megarbry <megarbry@gmail.com>
2020-05-25 15:03:32 -0600

    Adding project to repos

---
+++ src/components/journals/journal_list.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+import { JournalEntry } from './journal_entry';
+
+const rawJournalData = [
+    {title: 'Post One', content: 'Post content', status: 'draft'},
+    {title: 'Post Two', content: 'More post content', status: 'published'},
+    {title: 'Post Three', content: 'Another post content', status: 'published'},
+    {title: 'Post Four', content: 'One more post content', status: 'published'}
+]
+
+
+
+export default class JournalList extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            journalData: rawJournalData,
+            isOpen: true
+        }
+    }
+
+    clearEntries = () => {
+        this.setState({ journalData: [], isOpen: false });
+    }
+
+    showAllEntries = () => {
+        this.setState({ journalData: rawJournalData, isOpen: true });
+    }
+
+    toggleStatus = () => {
+        if(this.state.isOpen) {
+            this.setState({ journalData: [], isOpen: false })
+        } else {
+            this.setState({ journalData: rawJournalData, isOpen: true });
+        }
+    }
+
+    render() {
+        const journalEntries = this.state.journalData.map(journalEntry => {
+            return(
+            <div key={journalEntry.title}>
+                <JournalEntry 
+                    title={journalEntry.title}
+                    content={journalEntry.content}
+                />
+            </div>
+            )
+        })
+
+        return(
+            <div>
+                <h2>{this.props.heading}</h2>
+                {journalEntries}
+
+                <button onClick={this.clearEntries}>Clear journal entries</button>
+                <button onClick={this.showAllEntries}>Show all entries</button>
+                <button onClick={this.toggleStatus}>Toggle status</button>
+            </div>
+        );
+    }
+}
